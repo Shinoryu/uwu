883a6ab6be543a78140a86991346211a14785f55
Shinoryu <41242414+Shinoryu@users.noreply.github.com>
2019-04-04 23:18:47 -0400

    owo

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -10,7 +10,7 @@ var overviewerConfig = {
                 70
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1554433558, 
+            "last_rendertime": 1554434199, 
             "name": "Overviewer Render (lighting)", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -66,6 +66,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1554433695"
+        "cacheTag": "1554434286"
     }
 };
